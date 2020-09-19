79981dccd6fb4e75755fc4b79b3d46e7331b8f4f
jinkya2 <65954625+jinkya2@users.noreply.github.com>
2020-09-19 14:15:15 +0530

    Add files via upload

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+  tasks: string[];
+
+  constructor() {
+    this.tasks = [
+      'task1',
+      'task2'
+    ]
+  }
+
+  ngOnInit(): void {}
+}
